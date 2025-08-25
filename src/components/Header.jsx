b6c493075444d000f22361332ae3cb39bec6685b
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-25 16:00:29 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div id="logo">
+        <img src="./img/dc-logo.png" alt="Logo-header" />
+      </div>
+    </header>
+  )
+}
+
+export default Header
