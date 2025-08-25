d78d17e251135a173bf6543ef2817910c278b7ee
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-25 17:39:30 +0200

    main

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+      <div id="container">
+        <h2>-- Content goes here --</h2>
+      </div>
+    </main>
+  )
+}
+
+export default Main
