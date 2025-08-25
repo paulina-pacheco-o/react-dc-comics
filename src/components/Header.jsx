7a1ec6e163a6ce197dc23ec52325c7054caff6c9
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-25 17:00:43 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,22 @@ const Header = () => {
   return (
     <header>
       <div id="logo">
-        <img src="./img/dc-logo.png" alt="Logo-header" />
+        <img src="../img/dc-logo.png" alt="Logo-header" />
       </div>
+      <nav>
+        <ul>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a href="#" className="active">COMICS</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+        </ul>
+      </nav>
     </header>
   )
 }
