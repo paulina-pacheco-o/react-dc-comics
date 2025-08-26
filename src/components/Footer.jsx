a2185b6c8d56df22c70e7bb7522653cb82319307
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-26 15:59:35 +0200

    middle footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,48 @@ const Footer = () => {
   return (
     <footer>
       <div id="footerBlu">
-        <ul>
-          <li><img src="./img/buy-comics-digitalcomics.png" alt="Buy" />DIGITAL COMICS</li>
-          <li><img src="./img/buy-comics-merchandise.png" alt="Merchandise" />DC MERCHANDISE</li>
-          <li><img src="./img/buy-comics-subscriptions.png" alt="Subscription" />SUBSCRIPTION</li>
-          <li><img src="./img/buy-comics-shop-locator.png" alt="Shop" />COMIC SHOP LOCATOR</li>
-          <li><img src="./img/buy-dc-power-visa.svg" alt="Dc power" />DC POWER VISA</li>
-        </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col-100">
+              <div className="icons-list">
+                <div>
+                  <img src="./img/buy-comics-digital-comics.png" alt="Buy" />
+                  <span>DIGITAL COMICS</span>
+                  <img src="./img/buy-comics-merchandise.png" alt="Merchandise" />
+                  <span>DC MERCHANDISE</span>
+                  <img src="./img/buy-comics-subscriptions.png" alt="Subscription" />
+                  <span>SUBSCRIPTION</span>
+                  <img src="./img/buy-comics-shop-locator.png" alt="Shop" />
+                  <span>COMIC SHOP LOCATOR</span>
+                  <img src="./img/buy-dc-power-visa.svg" alt="Dc power" />
+                  <span>DC POWER VISA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="jumbotron">
+        <div className="container">
+          <div className="row">
+            <div className="col-15 list-footer">
+              <ul>
+                <h3>DC COMICS</h3>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+              </ul>
+            </div>
+            <div className="col-15"></div>
+            <div className="col-15"></div>
+            <div className="col-15"></div>
+            <div className="col-40 background-dc"></div>
+          </div>
+        </div>
       </div>
     </footer>
   )
