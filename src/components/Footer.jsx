febc5bd732bcefd72b3fcf8a29d6fb6ff34d14a5
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-26 09:14:15 +0200

    first section footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-
+      <div id="footerBlu">
+        <ul>
+          <li><img src="./img/buy-comics-digitalcomics.png" alt="Buy" />DIGITAL COMICS</li>
+          <li><img src="./img/buy-comics-merchandise.png" alt="Merchandise" />DC MERCHANDISE</li>
+          <li><img src="./img/buy-comics-subscriptions.png" alt="Subscription" />SUBSCRIPTION</li>
+          <li><img src="./img/buy-comics-shop-locator.png" alt="Shop" />COMIC SHOP LOCATOR</li>
+          <li><img src="./img/buy-dc-power-visa.svg" alt="Dc power" />DC POWER VISA</li>
+        </ul>
+      </div>
     </footer>
   )
 }
