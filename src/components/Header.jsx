b1cfde4c9fdacc20dcd10a2b322074da16a46386
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-27 18:03:32 +0200

    navbar header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,96 @@
 import React from 'react'
 
 const Header = () => {
+  const menu = [
+    {
+      id: 1,
+      label: "CHARACTERS",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 2,
+      label: "COMICS",
+      active: true,
+      href: "#"
+    },
+    {
+      id: 3,
+      label: "MOVIES",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 4,
+      label: "TV",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 5,
+      label: "GAMES",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 6,
+      label: "COLLECTIBLES",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 7,
+      label: "VIDEOS",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 8,
+      label: "FANS",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 9,
+      label: "NEWS",
+      active: false,
+      href: "#"
+    },
+    {
+      id: 10,
+      label: "SHOP",
+      active: false,
+      href: "#"
+    },
+  ]
   return (
     <header>
-      <div id="logo">
-        <img src="../img/dc-logo.png" alt="Logo-header" />
+      <div className="container">
+        <div className="row">
+          <div className="col-20">
+            <div id="logo">
+              <img src="../img/dc-logo.png" alt="Logo-header" />
+            </div>
+          </div>
+          <div className="col-80"><ul id='menu'>
+            {menu.map((link) => {
+              return (
+                <li><a href="#">CHARACTERS</a></li>
+              )
+            })}
+
+            <li><a href="#" className="active">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul></div>
+        </div>
       </div>
-      <nav className='menu'>
-        <ul>
-          <li><a href="#">CHARACTERS</a></li>
-          <li><a href="#" className="active">COMICS</a></li>
-          <li><a href="#">MOVIES</a></li>
-          <li><a href="#">TV</a></li>
-          <li><a href="#">GAMES</a></li>
-          <li><a href="#">COLLECTIBLES</a></li>
-          <li><a href="#">VIDEOS</a></li>
-          <li><a href="#">FANS</a></li>
-          <li><a href="#">NEWS</a></li>
-          <li><a href="#">SHOP</a></li>
-        </ul>
-      </nav>
     </header>
   )
 }
